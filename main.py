2aa0f6d5759981e43a7d4e91d167032529ede692
JakobCoding <jakechalmers86@gmail.com>
2022-09-26 16:25:32 +0800

    symbol values added

--- main.py
+++ main.py
@@ -14,9 +14,29 @@ symbol_count = {
     "D": 8
 }
 
+symbol_value = {
+    "A": 5,
+    "B": 4,
+    "C": 3,
+    "D": 2
+}
+
+def check_winnings(columns, lines, bet, values):
+    winnings = 0 
+    for line in range(lines):
+        symbol = colums[0][line]
+        for column in columns:
+            symbol_to_check = column[line]
+            if symbol != symbol_to_check:
+                break
+        else: 
+            winnings += values[symbol] * bet
+
+    return winnings
+
 def get_slot_machine_spin(rows, cols, symbols):
     all_symbols = []
-    for symbol, symbol_count in symbol.items():
+    for symbol, symbol_count in symbols.items():
         for _ in range(symbol_count):
             all_symbols.append(symbol)
 
@@ -37,10 +57,11 @@ def print_slot_machine(columns):
     for row in range(len(columns[0])):
         for i, column in enumerate(columns):
             if i != len(columns) - 1:
-                print(column[row], "|")
+                print(column[row], end=" | ")
             else:
-                print (column[row])
+                print (column[row], end="")
 
+        print()
 
 
 
@@ -106,6 +127,9 @@ def main():
         f"You are betting ${bet} on {lines} lines. Total bet is equal to: ${total_bet}")
 
 
-    
+slots = get_slot_machine_spin(ROWS, COLS, symbol_count)  
+print_slot_machine(slots)
+
+
 main()
 
